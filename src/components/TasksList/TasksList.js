08d3d332703c66dab93f69264aa264f84e55b4e9
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-20 21:35:40 +0100

    1

--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { store } from '../../store/store';
 
-const TasksList = (props) => (
-    <ul>
-        {props.store.map((item) => (
-            <li key={Math.random()}>{item}</li>
-        ))}
-    </ul>
-);
+const TasksList = (props) => {
+    console.log(props);
+    return (
+        <ul>
+            {props.store.map((item) => (
+                <li key={Math.random()}>{item}</li>
+            ))}
+        </ul>
+    );
+};
+
+store.subscribe(TasksList);
 
 export default TasksList;
