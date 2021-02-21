8fab5d8ce87ee78030567bd8db74b4965f7db3a9
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-21 18:20:35 +0100

    dupa

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 
 const Task = ({ title, id }) => {
     const dispatch = useDispatch();
-
+    console.log('dupa');
     return <div onClick={() => dispatch(deleteTodo(id))}>{title}</div>;
 };
 
