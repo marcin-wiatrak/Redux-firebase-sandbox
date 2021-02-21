94722df33f74b803f4d4085e1941c2b7b828b99d
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-21 17:55:32 +0100

    2

--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { store } from '../../store/store';
-
+import { useSelector } from 'react-redux';
 const TasksList = (props) => {
     console.log(props);
+    const todos = useSelector((state) => state);
     return (
         <ul>
             {props.store.map((item) => (
